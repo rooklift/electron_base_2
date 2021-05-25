a1bd01019e38bc53efb0e25a0f1c3dc53f5215e3
rooklift <16438795+rooklift@users.noreply.github.com>
2021-05-25 13:01:50 +0100

    back to simple settings, and add "quit"

--- modules/config_io.js
+++ modules/config_io.js
@@ -19,27 +19,11 @@ exports.filepath = electron.app ?
 
 exports.config = {};
 
-exports.defaults_classified = {
-
-	// It is OK for something to be in multiple classications.
-
-	no_immediate_effect: {
-		"width": 1024,
-		"height": 768,
-	},
-
-	alerters: {
-		"foo": true,
-	},
+exports.defaults = {
+	"width": 1024,
+	"height": 768,
+	"foo": true,
 };
-
-exports.defaults = {};
-
-for (let cl of Object.keys(exports.defaults_classified)) {
-	for (let key of Object.keys(exports.defaults_classified[cl])) {
-		exports.defaults[key] = exports.defaults_classified[cl][key];
-	}
-}
 
 // ------------------------------------------------------------------------------------------------
 
--- modules/hub.js
+++ modules/hub.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const {ipcRenderer} = require("electron");
 const {defaults_classified} = require("./config_io");
 
 exports.new_hub = function() {
@@ -10,33 +11,21 @@ exports.new_hub = function() {
 let hub_props = {
 
 	set: function(key, value) {
+
 		config[key] = value;
-		save_config();
-		this.take_followup_actions([key]);
+
+		switch (key) {		// Any followup actions needed for individual keys... most commonly some redraw will be required when a setting changes.
+
+		case "foo":
+			alert("A setting change triggered this alert.");
+			break;
+		}
+
 	},
 
-	take_followup_actions: function(keys) {
-
-		if (Array.isArray(keys) === false) {
-			throw "take_followup_actions(): bad call";
-		}
-
-		let hits = {};
-		let classifiers = Object.keys(defaults_classified);
-
-		for (let key of keys) {
-			for (let cl of classifiers) {
-				if (defaults_classified[cl][key] !== undefined) {
-					hits[cl] = true;
-				}
-			}
-		}
-
-		if (hits.alerters) {
-			alert("A setting change triggered this alert.");
-		}
-
-		// etc - most commonly some redraw will be required when a setting changes.
+	quit: function() {
+		save_config();									// As long as we use the sync save, this will complete before we
+		ipcRenderer.send("terminate");					// send "terminate". Not sure about results if that wasn't so.
 	},
 
 };
